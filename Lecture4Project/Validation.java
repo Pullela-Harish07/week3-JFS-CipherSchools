1cb84b65101970a6d426595d10d9117a8c8cad60
Harish <harishchowdary616@gmail.com>
2024-02-13 12:42:00 +0530

    version 1

---
+++ Lecture4Project/Validation.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.codejava.validation;
+
+/**
+ *
+ * @author Harish
+ */
+public class Validation {
+
+    public static void main(String[] args) {
+        NewJFrame app = new NewJFrame();
+        app.setVisible(true);
+    }
+}
